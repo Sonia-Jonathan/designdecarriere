6c9adb0b28a162f6c36196326eff4da006a95ed3
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2025-05-20 17:17:06 +0200

    fix css mercy

--- merci-sub.php
+++ merci-sub.php
@@ -5,7 +5,10 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-
+<?php
+/* Template Name: Landing Page */
+get_header('landing');
+?>
 
 
 
@@ -24,17 +27,37 @@
     </main>
 
 
-
-
-
-
-
-
-
-
-
-
-
+    <footer class="footer-site">
+        <div class="footer-wrapper">
+            <div class="footer-column">
+                <?php
+                wp_nav_menu([
+                    'menu' => 'Footer Colonne 1',
+                    'container' => false,
+                    'menu_class' => 'footer-menu',
+                ]);
+                ?>
+            </div>
+            <div class="footer-column">
+                <?php
+                wp_nav_menu([
+                    'menu' => 'Footer Colonne 2',
+                    'container' => false,
+                    'menu_class' => 'footer-menu',
+                ]);
+                ?>
+            </div>
+            <div class="footer-column">
+                <?php
+                wp_nav_menu([
+                    'menu' => 'Footer Colonne 3',
+                    'container' => false,
+                    'menu_class' => 'footer-menu',
+                ]);
+                ?>
+            </div>
+        </div>
+    </footer>
 
 
 

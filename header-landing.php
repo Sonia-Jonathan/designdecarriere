b761cc3492d428ed22f17a1d67eab63028d73269
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2025-05-26 19:50:31 +0200

    add norobot merci change page

--- header-landing.php
+++ header-landing.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <?php if (is_page('merci-sub')) : ?>
+    <?php if (is_page('merci-pour-votre-inscription')) : ?>
         <meta name="robots" content="noindex, follow">
     <?php endif; ?>
 </head>

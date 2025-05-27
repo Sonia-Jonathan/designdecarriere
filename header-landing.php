257270ce766670eec293b4793d11e2ffb332669e
Sonia-Jonathan <soniajonathan2108.pro@gmail.com>
2025-05-28 01:13:40 +0200

    remove robot

--- header-landing.php
+++ header-landing.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <title><?php the_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta name="robots" content="noindex, nofollow"> -->
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/landing.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,9 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <?php if (is_page('merci-pour-votre-inscription')) : ?>
-        <meta name="robots" content="noindex, follow">
-    <?php endif; ?>
 </head>
 
 <body>
